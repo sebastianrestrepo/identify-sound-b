ac916f86295b602a62bf17b42d684fb76a4de02a
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-09-17 12:33:33 -0500

    klsadfjsafkljs

--- src/components/IdentifySoundB/IdentifySoundB.tsx
+++ src/components/IdentifySoundB/IdentifySoundB.tsx
@@ -18,8 +18,8 @@ import { relative, isAbsolute } from 'path';
 
     constructor(props) {
         super(props);
- 
-      }
+
+    }
 
     render() {
         return <section >
@@ -42,36 +42,44 @@ import { relative, isAbsolute } from 'path';
                 }} />
             </div>
             {(this.showTagOption)
-            ?<div className="card-section" id="card-for-tagging" style={{
-                top: this.posYCard,
-                left: this.posXCard,
-              }}>
-                <span className="description-items-row">
-                    <p><span className="text-title">Fecha:</span> 28 Julio 2019</p>
-                    <p><span className="text-title">Hora:</span> 06:36 AM</p>
-                    <p><span className="text-title">Duración:</span> 5s</p>
-                </span>
-                <img src="./assets/Birds.png" alt=""></img>
-                <span className="input-row card-item">
-                    <p className="text-title"> Nombre Común:</p> <input type="text" />
-                    <p className="text-title"> Nombre Científico:</p> <input type="text" />
-                </span>
-                <span className="input-row card-item">
-                    <p className="text-title">Tipo de canto:</p>
-                    <select>
-                        <option>Canto</option>
-                        <option>Llamador</option>
-                        <option>Cortejo</option>
-                    </select>
-                </span>
-                <p className="green-link card-item">Agregar anotación o comentario</p>
-                <label><input type="checkbox" id="myCheck"></input>No estoy seguro de que especie es</label>
-                <button className="green-button" style={{}}>Etiquetar</button>
-                <button className="green-button" style={{
-                    backgroundColor: 'orange'
-                }}>Pendiente</button>
-            </div>
-            :''}
+                ? <div className="card-section" id="card-for-tagging" style={{
+                    top: this.posYCard,
+                    left: this.posXCard,
+                }}>
+                    <span className="description-items-row">
+                        <p><span className="text-title">Fecha:</span> 28 Julio 2019</p>
+                        <p><span className="text-title">Hora:</span> 06:36 AM</p>
+                        <p><span className="text-title">Duración:</span> 5s</p>
+                    </span>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column'
+                    }}>
+                        <p>Especies estudiando</p>
+                        <img src="./assets/Birds.png" alt="" className="img-birds" width="246"></img>
+                    </div>
+                    <span className="input-row card-item">
+                        <span className="input-col card-item"> <p className="text-title"> Nombre Común:</p> <input type="text" /></span>
+                        <span className="input-col card-item"> <p className="text-title"> Nombre Científico:</p> <input type="text" /></span>
+                    </span>
+                    <span className="input-row card-item">
+                        <p className="text-title">Tipo de canto:</p>
+                        <select>
+                            <option>Canto</option>
+                            <option>Llamador</option>
+                            <option>Cortejo</option>
+                        </select>
+                    </span>
+                    <span className="input-col card-item"> <p className="text-title"> Anotación/Comentario:</p> <input type="text" /></span>
+                    <label><input type="checkbox" id="myCheck"></input>No estoy seguro de que especie es</label>
+                    <div className="btns">
+                        <button className="green-button" style={{}}>Etiquetar</button>
+                        <button className="green-button" style={{
+                            backgroundColor: 'orange'
+                        }}>Pendiente</button>
+                    </div>
+                </div>
+                : ''}
             <div className="card-section">
                 <p className="hint-text">¿Alguna duda sobre el canto? Verifica con cantos de Xeno-Canto</p>
                 <span className="input-row search card-item">
